9d124c07527171cf04d237639273e07125e34489
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-12-18 13:17:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-const images = document.querySelectorAll('img');
-let draggedElement = null;
 
-images.forEach((image) => {
-    image.addEventListener('dragstart', handleDragStart);
-    image.addEventListener('dragover', handleDragOver);
-    image.addEventListener('drop', handleDrop);
-});
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
 
-function handleDragStart(event) {
-    draggedElement = event.target;
+const images = document.querySelectorAll(".image");
+
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
 }
 
-function handleDragOver(event) {
-    event.preventDefault();
+function allowDrop(e) {
+  e.preventDefault();
 }
 
-function handleDrop(event) {
-    event.preventDefault();
-    if (draggedElement && draggedElement !== event.target) {
-        const tempContent = draggedElement.src;
-        draggedElement.src = event.target.src;
-        event.target.src = tempContent;
+function drop(e) {
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  let nodelist = document.getElementById("parent").childNodes;
+  console.log(data, e.target.id);
+  for (let i = 0; i < nodelist.length; i++) {
+    if (nodelist[i].id == data) {
+      dragindex = i;
     }
+  }
+
+  dragdrop(clone);
+
+  document
+    .getElementById("parent")
+    .replaceChild(document.getElementById(data), e.target);
+
+  document
+    .getElementById("parent")
+    .insertBefore(
+      clone,
+      document.getElementById("parent").childNodes[dragindex]
+    );
 }
+
+const dragdrop = (image) => {
+  image.ondragstart = drag;
+  image.ondragover = allowDrop;
+  image.ondrop = drop;
+};
+
+images.forEach(dragdrop);
